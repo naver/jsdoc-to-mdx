cac9dc5ba499814dcdcdcd0ff5811c19310b09e5
WoodNeck <saiyu915@naver.com>
2022-03-21 17:21:07 +0900

    chore(release): release 1.1.2

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import banner2 from 'rollup-plugin-banner2'
+
+import packageFile from "./package.json";
 
 export default {
   input: "src/generate.ts",
@@ -10,6 +10,7 @@ export default {
     format: "cjs",
     name: "jsdoc-to-mdx"
   },
-  plugins: [typescript(), resolve(), commonjs(), banner2(() => "#!/usr/bin/env node\n")]
+  plugins: [typescript(), banner2(() => "#!/usr/bin/env node\n")],
+  external: [...Object.keys(packageFile.dependencies), "jsdoc-parse"]
 }
 
